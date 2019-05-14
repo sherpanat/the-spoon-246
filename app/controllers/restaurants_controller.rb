ec9d64e15802e70ccfdd8bbe0fc12740bc9a999a
Nathan Huberty <nathan.huberty@orange.fr>
2019-05-14 12:08:39 +0200

    App over

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,46 @@
+class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(set_restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurants_path
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.update(set_restaurant_params)
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant.destroy
+
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def set_restaurant_params
+    params.require(:restaurant).permit(:name, :address, :stars)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # VERB PATH, to: 'CONTROLLER#ACTION'
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
 end
